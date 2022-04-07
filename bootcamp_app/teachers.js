6ca9ed2f1cf12c27ec9e4e189f7406341a7e714a
Tanner Farrelly <tannerfarrelly@gmail.com>
2022-04-06 20:10:16 -0400

    add safety features for sql injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -10,20 +10,24 @@ const pool = new Pool({
 const params = process.argv.slice(2);
 const cohort = params[0];
 
-pool.query(`
+const queryString = `
 SELECT distinct teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name like '%${cohort}%'
+WHERE cohorts.name like $1
 ORDER BY teachers.name;
-`)
-.then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
+`;
+const queryParams = [cohort];
+
+
+pool.query(queryString, queryParams)
+  .then(res => {
+    res.rows.forEach(row => {
+      console.log(`${row.cohort}: ${row.teacher}`);
+    });
   })
-})
-.catch(err => console.error('query error', err.stack))
-.finally(() => pool.end());
+  .catch(err => console.error('query error', err.stack))
+  .finally(() => pool.end());
 
